c0a4cec1192f131c07056b9c60268528d602701d
Vinh <73513296+VinnyBui@users.noreply.github.com>
2021-02-10 18:52:20 -0800

    Update list_manipulation.py
    Combined the list, sorted in alphabetical order, and print out the first half of the alphabet. Also printed out the reversed sorted list without using reverse() function.

--- list_manipulation.py
+++ list_manipulation.py
@@ -14,3 +14,11 @@ fruits = ["grape", "mango", "nectarine", "pineapple", "banana",
 vegetables = ["zucchini", "asparagus", "kale", "spinach", "broccoli",
               "celery", "beets", "bok choy", "brussels sprouts", "arugula"]
 
+newList = fruits + vegetables
+print("There are",len(newList),"elements in the combined list")
+newList.sort()
+for food in newList:
+  if food.startswith(("a","b","c","d","e","f","g","h","i","j","k","l","m")):
+    print(food)
+reverseList=newList[::-1]
+print(reverseList)
